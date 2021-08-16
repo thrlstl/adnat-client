653161952b9ae35f6bf825029d30cb84bac0e251
thrlstl <matthewsteeleonline@gmail.com>
2021-08-15 21:42:46 -0500

    JWT Auth ready w/ token

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-// import { useEffect } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import Auth from './components/Auth/Auth'
 import Header from './components/Header'
 import HomeStack from './components/HomeStack';
 
-// import API from '../src/components/Auth/API'
-// const API_URL = API()
+import API from '../src/components/Auth/API'
+const API_URL = API()
 
 function App() {
 
-  // useEffect(() => {
-  //   let token = localStorage.getItem('token')
-  //   if (token) {
-  //     fetch(`${API_URL}auth`, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Authorization': `Bearer ${token}`
-  //       }
-  //     })
-  //     .then(resp => resp.json())
-  //     .then(result => {
-  //       console.log(result)
-  //     })
-  //   }
-  // }, [])
+  useEffect(() => {
+    let JWT_TOKEN = localStorage.getItem('token')
+    if (JWT_TOKEN) {
+      fetch(`${API_URL}auto_login`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${JWT_TOKEN}`
+        }
+      })
+      .then(resp => resp.json())
+      .then(result => {
+        console.log(result)
+      })
+    }
+  }, [])
 
   return (
     <div className="App">
